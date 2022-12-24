bad633a077d85fc799fbe8ca6e42e8829b78e1e6
Bruno Morais <brunomoraisads@gmail.com>
2022-12-24 14:36:36 -0300

    Alteração na criação de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router()
+const Article = require("../articles/models/Article")
+const Category = require("../categories/models/Category")
 
 router.get("/admin/articles/new", (req, res) => {
-    res.render("admin/articles/new")
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {
+            categories: categories
+        })
+    })
 })
 
 module.exports = router
