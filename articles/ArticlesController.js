8488cfdacd5bfc33550ec506857c76fbe3fd6965
Bruno Morais <brunomoraisads@gmail.com>
2022-12-24 15:14:32 -0300

    Implementação da rota principal dos articles, implementação da rota de salvar um article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,34 @@
 const express = require("express")
 const router = express.Router()
 const Article = require("../articles/models/Article")
+const slugify = require("slugify")
 const Category = require("../categories/models/Category")
+
+//ROTAS ARTICLES
+
+router.get("/admin/articles", (req, res)=>{
+    res.render("admin/articles")
+})
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {
             categories: categories
         })
+    })
+})
+
+router.post("/articles/save", (req,res) =>{
+    const {title, body, category} = req.body
+
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/articles")
     })
 })
 
